6963f3c5cb7647d6bf8f3b254e1c2857da68d709
Dongil Chang <dongil.chang@apex.ai>
2025-08-27 12:46:09 +0200

    Remove unnecessary script and icons

--- _bazel_build_html_requirements.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#
-# Copyright 2019 Apex.AI, Inc.
-# All rights reserved.
-
-set -ex
-bazel run @apex//apex_internal/tools/apex_doc_tools:apex_doc -- requirements
-
-xdg-open doc/internal/build/index.html&
